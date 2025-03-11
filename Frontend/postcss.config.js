47a19e97b32aca248d0992121c88650c95ec621f
sankarkiran-02 <sankarkiran5758@gmail.com>
2025-03-12 00:01:05 +0530

    Nextjs

--- Frontend/postcss.config.js
+++ Frontend/postcss.config.js
@@ -1,9 +1,8 @@
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
 
-export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
-};
+module.exports = {
+    plugins: {
+      '@tailwindcss/postcss': {},
+      autoprefixer: {},
+    },
+  }
+  
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+
+module.exports = {
     content: [
-      "./index.html",
-      "./src/**/*.{js,jsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
